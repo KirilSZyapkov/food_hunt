7a364ef4b0d3846087d050d5ca83db6cbf7eb91e
Kiril Zyapkov <ziapkov@gmail.com>
2023-09-15 10:45:51 +0300

    Adding dynamic styles.

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -1,13 +1,37 @@
-import React from "react";
+"use client";
+
+import React, { useEffect } from "react";
 
 import styles from "./Navbar.module.css";
 
 function Navbar() {
+  useEffect(() => {
+    window.addEventListener("scroll", scrollFunction);
+
+    function scrollFunction() {
+      if (
+        document.body.scrollTop > 80 ||
+        document.documentElement.scrollTop > 80
+      ) {
+        document.getElementById("navbar")?.classList.add("afix");
+
+        document.getElementById("navLogo")?.classList.add("afix_logo");
+        document.getElementById("navTxt")?.classList.add("afix_txt");
+        console.log("a");
+      } else {
+        console.log("b");
+        document.getElementById("navbar")?.classList.remove("afix");
+
+        document.getElementById("navLogo")?.classList.remove("afix_logo");
+        document.getElementById("navTxt")?.classList.remove("afix_txt");
+      }
+    }
+  }, []);
+
   return (
     <nav
-      className={`flex justify-between items-center w-full p-5 ${
-        (styles.custom_navbar, styles.fixed_top, styles.navbar_expand_lg)
-      }`}
+      id="navbar"
+      className={`flex justify-between items-center w-full p-5 ${styles.custom_navbar} ${styles.fixed_top} ${styles.navbar_expand_lg}`}
     >
       <div className="flex basis-0 grow items-center">
         <div className="flex items-center gap-5 text-white font-bold text-base">
@@ -17,8 +41,15 @@ function Navbar() {
           <a href="#">Book-Table</a>
         </div>
         <a href="#" className={styles.nav_brand}>
-          <img className={styles.nav_logo} src="/logo.svg" alt="logo" />
-          <span className={styles.nav_brand_text}>Food Hunt</span>
+          <img
+            id="navLogo"
+            className={styles.nav_logo}
+            src="/logo.svg"
+            alt="logo"
+          />
+          <span id="navTxt" className={styles.nav_brand_text}>
+            Food Hunt
+          </span>
         </a>
         <div className="flex items-center gap-5 text-white font-bold text-base">
           <a href="#">Blog</a>
