a287650e0756b014a953eeb5cbb6649863b246f9
Kiril Zyapkov <ziapkov@gmail.com>
2023-09-16 17:57:12 +0300

    Adding more styles to landing page's text.

--- app/components/landing/Landing.tsx
+++ app/components/landing/Landing.tsx
@@ -5,10 +5,10 @@ import styles from './Landing.module.css';
 function Landing() {
   return (
     <div className={styles.header}>
-      <div className='text-white text-center'>
+      <div className='text-current text-center text-[5.5rem]'>
         <h1 className='font-bold my-4' >Food Hunt</h1>
-        <h2>Always fresh &amp; Delightful</h2>
-        <a href="#gallary">View Our gallary</a>
+        <h2 className='text-[3.5rem] mb-[3rem]'>Always fresh &amp; Delightful</h2>
+        <a className={`${styles.btn} ${styles.btn_lg} text-white bg-[#ff214f] border-[#ff214f]`} href="#gallary">View Our gallary</a>
       </div>
     </div>
   )
