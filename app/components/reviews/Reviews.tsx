b62d616f3c5032903b4943d0ebcec7c8e2c9ac7f
Kiril Zyapkov <ziapkov@gmail.com>
2023-09-23 18:03:07 +0300

    Working on review's section.

--- app/components/reviews/Reviews.tsx
+++ app/components/reviews/Reviews.tsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import React from "react";
 
 function Reviews() {
   return (
-    <div>Reviews</div>
-  )
+    <section>
+      <h2>REVIEWS</h2>
+      <div>
+        <div>
+          <div>
+            <h3>John Doe</h3>
+            <h6>Web Designer</h6>
+            <div>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum
+                nobis eligendi, quaerat accusamus ipsum sequi dignissimos
+                consequuntur blanditiis natus. Aperiam!
+              </p>
+            </div>
+          </div>
+          <div>
+            <h3>John Doe</h3>
+            <h6>Painter</h6>
+            <div>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum
+                nobis eligendi, quaerat accusamus ipsum sequi dignissimos
+                consequuntur blanditiis natus. Aperiam!
+              </p>
+            </div>
+          </div>
+          <div>
+            <h3>John Doe</h3>
+            <h6>Musition</h6>
+            <div>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum
+                nobis eligendi, quaerat accusamus ipsum sequi dignissimos
+                consequuntur blanditiis natus. Aperiam!
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
 
-export default Reviews
+export default Reviews;
