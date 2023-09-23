8ea8222f7a2b19e3d75ba7c077e7071de1508a75
Kiril Zyapkov <ziapkov@gmail.com>
2023-09-23 19:08:17 +0300

    Adding styles to review cards.

--- app/components/reviews/Reviews.tsx
+++ app/components/reviews/Reviews.tsx
@@ -1,12 +1,13 @@
 import React from "react";
+import styles from "./Reviews.module.css";
 
 function Reviews() {
   return (
-    <section>
-      <h2>REVIEWS</h2>
-      <div>
-        <div>
-          <div>
+    <section className="min-h-[500px] flex flex-col justify-center text-white bg-[#343a40] w-full px-[15px]">
+      <h2 className="text-7xl text-center my-[3rem] font-medium">REVIEWS</h2>
+      <div className={styles.row}>
+        <div className={styles.col}>
+          <div className="relative text-center">
             <h3>John Doe</h3>
             <h6>Web Designer</h6>
             <div>
@@ -17,8 +18,11 @@ function Reviews() {
               </p>
             </div>
           </div>
-          <div>
-            <h3>John Doe</h3>
+        </div>
+
+        <div className={styles.col}>
+          <div className="relative text-center">
+            <h3>Steve Thomas</h3>
             <h6>Painter</h6>
             <div>
               <p>
@@ -28,8 +32,11 @@ function Reviews() {
               </p>
             </div>
           </div>
-          <div>
-            <h3>John Doe</h3>
+        </div>
+
+        <div className={styles.col}>
+          <div className="relative text-center">
+            <h3>Miranda Joy</h3>
             <h6>Musition</h6>
             <div>
               <p>
