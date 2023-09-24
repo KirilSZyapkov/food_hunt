fd01af7f878ba7d46c79b30d9611016f3ffd9b2f
Kiril Zyapkov <ziapkov@gmail.com>
2023-09-24 22:52:34 +0300

    Finishing the designe.

--- app/components/footer/Footer.tsx
+++ app/components/footer/Footer.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <section className='text-center bg-[#343a40] border-t border-t-[#495057]'>
+      <p className='text-[#adb5bd] py-[1rem] text-[80%] font-normal'>© Copyright 2023 Made by <a className='text-[#F85C70]' href='https://k-websolutions.netlify.app/'>K-Websolutions</a></p>
+    </section>
   )
 }
 
