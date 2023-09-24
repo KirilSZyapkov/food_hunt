f31acd8c31967b46b5babc5a182ea031e677e52a
Kiril Zyapkov <ziapkov@gmail.com>
2023-09-24 19:36:36 +0300

    Adding the rest elements from contacts.

--- app/components/contactUs/ContactUs.tsx
+++ app/components/contactUs/ContactUs.tsx
@@ -4,55 +4,73 @@ import React, { useEffect } from "react";
 import styles from "./ContactUs.module.css";
 
 function ContactUs() {
-
-  useEffect(()=>{
+  useEffect(() => {
     function renderMap() {
       loadScript(
         "https://maps.googleapis.com/maps/api/js?key=AIzaSyD4L8RDcj8kbyrnKW8IOdNcaONvuGAYUGY&callback=initMap&libraries=&v=weekly"
       );
+
       window.initMap = initMap;
     }
     renderMap();
-  
+
     function initMap() {
       // The location of Uluru
-  
+
       const uluru = { lat: 44.43629, lng: 26.103575 };
       // The map, centered at Uluru
       const map = new window.google.maps.Map(document.getElementById("map"), {
         zoom: 8,
         center: uluru,
       });
-  
+
       const marker = new window.google.maps.Marker({
         position: uluru,
         map: map,
       });
-  
+
       return {
         map,
         marker,
       };
     }
-  
+
     function loadScript(url: string) {
-      let index = window.document.getElementsByTagName('script')[0];
-      let script = window.document.createElement('script');
+      let index = window.document.getElementsByTagName("script")[0];
+      let script = window.document.createElement("script");
       script.src = url;
       script.async = true;
       script.defer = true;
       index.parentNode?.insertBefore(script, index);
     }
-  },[]);
- 
+  }, []);
 
   return (
     <section>
       <div>
+        <div>
+          <h3>FIND US</h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sit,
+            laboriosam doloremque odio delectus, sunt magnam laborum impedit
+            molestiae, magni quae ipsum, ullam eos! Alias suscipit impedit et,
+            adipisci illo quam.
+          </p>
+          <div>
+            <p>
+              <span>12345 Fake ST NoWhere, AB Country</span>
+            </p>
+            <p>
+              <span>{"(123) 456-7890"}</span>
+            </p>
+            <p>
+              <span>info@website.com</span>
+            </p>
+          </div>
+        </div>
         <div className={styles.map_container}>
           <div className={styles.map} id="map"></div>
         </div>
-        <div></div>
       </div>
     </section>
   );
