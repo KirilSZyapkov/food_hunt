f400e5c119cff811567adc0847f60afba545621d
Kiril Zyapkov <ziapkov@gmail.com>
2023-09-24 20:30:02 +0300

    Finishing the contact section.

--- app/components/contactUs/ContactUs.tsx
+++ app/components/contactUs/ContactUs.tsx
@@ -46,26 +46,31 @@ function ContactUs() {
   }, []);
 
   return (
-    <section>
+    <section className="w-full min-h-[300px] flex flex-wrap flex-coll justify-center bg-[#343a40] text-white border-t border-t-[#495057] px-[15px] mx-auto texte-center">
       <div>
-        <div>
-          <h3>FIND US</h3>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sit,
-            laboriosam doloremque odio delectus, sunt magnam laborum impedit
-            molestiae, magni quae ipsum, ullam eos! Alias suscipit impedit et,
-            adipisci illo quam.
-          </p>
-          <div>
-            <p>
+        <div className="flex flex-col">
+          <div className="flex flex-col text-center">
+            <h3 className="text-7xl text-center my-[3rem] font-medium">FIND US</h3>
+            <p className="text-start mb-[25px]">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sit,
+              laboriosam doloremque odio delectus, sunt magnam laborum impedit
+              molestiae, magni quae ipsum, ullam eos! Alias suscipit impedit et,
+              adipisci illo quam.
+            </p>
+          </div>
+          <div className={styles.contat_cards}>
+            <div className="text-center">
+              <h3 className="text-3xl text-center mb-[10px] font-medium">ADDRESS</h3>
               <span>12345 Fake ST NoWhere, AB Country</span>
-            </p>
-            <p>
+            </div>
+            <div className="text-center">
+              <h3 className="text-3xl text-center mb-[10px] font-medium">PHONE</h3>
               <span>{"(123) 456-7890"}</span>
-            </p>
-            <p>
-              <span>info@website.com</span>
-            </p>
+            </div>
+            <div className="text-center">
+              <h3 className="text-3xl text-center mb-[10px] font-medium">EMAIL</h3>
+              <a href="mailto:info@website.com">info@website.com</a>
+            </div>
           </div>
         </div>
         <div className={styles.map_container}>
