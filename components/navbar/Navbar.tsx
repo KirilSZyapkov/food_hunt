4b7dbc5d7db5153533b0cb1218d1b65b9fb82c61
Kiril Zyapkov <ziapkov@gmail.com>
2023-09-26 23:45:31 +0300

    Re-styling header bar.

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -1,48 +1,58 @@
 "use client";
 
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 import styles from "./Navbar.module.css";
 
 function Navbar() {
+  const [afix, setAfix] = useState("");
+  const [afixLogo, setAfixLogo] = useState("");
+  const [afixTxt, setAfixTxt] = useState("");
+
   useEffect(() => {
     window.addEventListener("scroll", scrollFunction);
 
-    function scrollFunction() {
-      if (
-        document.body.scrollTop > 80 ||
-        document.documentElement.scrollTop > 80
-      ) {
-        document.getElementById("navbar")?.classList.add('afix');
-        document.getElementById("navLogo")?.classList.add("afix_logo");
-        document.getElementById("navTxt")?.classList.add("afix_txt");
-        
-      } else {
-        
-        document.getElementById("navbar")?.classList.remove("afix");
-        document.getElementById("navLogo")?.classList.remove("afix_logo");
-        document.getElementById("navTxt")?.classList.remove("afix_txt");
-      }
-    }
+    return () => {
+      window.removeEventListener("scroll", scrollFunction);
+    };
   }, []);
+  function scrollFunction() {
+    if (
+      document.body.scrollTop > 80 ||
+      document.documentElement.scrollTop > 80
+    ) {
+      setAfix("afix");
+      setAfixLogo("afix_logo");
+      setAfixTxt("afix_txt");
+    } else {
+      setAfix("");
+      setAfixLogo("");
+      setAfixTxt("");
+    }
+  }
 
   return (
-    <header
-      id="navbar"
-      className={styles.header_menu}
-    >
+    <header className={`${afix} ${styles.header_menu}`}>
       <div className={styles.header_container}>
-        <div className="flex items-center gap-5 text-white font-bold text-base">
-          <a className={styles.link} href="#">Home</a>
-          <a className={styles.link} href="#">About</a>
-          <a className={styles.link} href="#">Gallery</a>
-          <a className={styles.link} href="#">Book-Table</a>
+        <div className="flex items-center gap-5 font-bold text-base">
+          <a className={styles.link} href="#">
+            Home
+          </a>
+          <a className={styles.link} href="#">
+            About
+          </a>
+          <a className={styles.link} href="#">
+            Gallery
+          </a>
+          <a className={styles.link} href="#">
+            Book-Table
+          </a>
         </div>
         <a href="#" className={styles.nav_brand}>
           <img
             id="navLogo"
             className={styles.nav_logo}
-            src="/logo.svg"
+            src="./logo.svg"
             alt="logo"
           />
           <span id="navTxt" className={styles.nav_brand_text}>
@@ -50,14 +60,18 @@ function Navbar() {
           </span>
         </a>
         <div className="flex items-center gap-5 text-white font-bold text-base">
-          <a className={styles.link} href="#">Blog</a>
-          <a className={styles.link} href="#">Reviews</a>
-          <a className={styles.link} href="#">Contact Us</a>
+          <a className={styles.link} href="#">
+            Blog
+          </a>
+          <a className={styles.link} href="#">
+            Reviews
+          </a>
+          <a className={styles.link} href="#">
+            Contact Us
+          </a>
         </div>
       </div>
-      <div className={styles.header_burger_menu}>
-    burger
-      </div>
+      <div className={styles.header_burger_menu}>burger</div>
     </header>
   );
 }
