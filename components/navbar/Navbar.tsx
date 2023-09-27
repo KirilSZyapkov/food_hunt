58f11a984b71e35e876c8a60f2a5aacc09d8546c
Kiril Zyapkov <ziapkov@gmail.com>
2023-09-27 22:04:27 +0300

    Designing the burger menu.

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -32,7 +32,7 @@ function Navbar() {
   }
 
   return (
-    <header className={`${styles.header_menu} ${(afix ? styles.afix : "")}`}>
+    <header className={`${styles.header_menu} ${afix ? styles.afix : ""}`}>
       <div className={styles.header_container}>
         <div className="flex items-center gap-5 font-bold text-base">
           <a className={styles.link} href="#">
@@ -51,11 +51,16 @@ function Navbar() {
         <a href="#" className={styles.nav_brand}>
           <img
             id="navLogo"
-            className={`${styles.nav_logo} ${(afixLogo? styles.afix_logo : "")}`}
+            className={`${styles.nav_logo} ${afixLogo ? styles.afix_logo : ""}`}
             src="./logo.svg"
             alt="logo"
           />
-          <span id="navTxt" className={`${styles.nav_brand_text} ${afixTxt? styles.afix_txt : ""}`}>
+          <span
+            id="navTxt"
+            className={`${styles.nav_brand_text} ${
+              afixTxt ? styles.afix_txt : ""
+            }`}
+          >
             Food Hunt
           </span>
         </a>
@@ -71,7 +76,18 @@ function Navbar() {
           </a>
         </div>
       </div>
-      <div className={styles.header_burger_menu}>burger</div>
+      <div className={styles.header_burger_menu_container}>
+        <ul className={styles.header_burger_menu}>
+          <li className="text-medium text-xl py-[10px] hover:text-[#ff214f]"><a className={styles.link} href="#">            Home          </a></li>
+          <li className="text-medium text-xl py-[10px] hover:text-[#ff214f]"><a className={styles.link} href="#">            About          </a></li>
+          <li className="text-medium text-xl py-[10px] hover:text-[#ff214f]"><a className={styles.link} href="#">            Gallery          </a></li>
+          <li className="text-medium text-xl py-[10px] hover:text-[#ff214f]"><a className={styles.link} href="#">            Book-Table          </a></li>
+          <li className="text-medium text-xl py-[10px] hover:text-[#ff214f]"><a className={styles.link} href="#">            Blog          </a></li>
+          <li className="text-medium text-xl py-[10px] hover:text-[#ff214f]"><a className={styles.link} href="#">            Reviews          </a></li>
+          <li className="text-medium text-xl py-[10px] hover:text-[#ff214f]"><a className={styles.link} href="#">            Contact Us          </a></li>
+        </ul>
+        <span className=" text-white hover:text-black text-4xl fixed right-5 top-5 cursor-pointer">&times;</span>
+      </div>
     </header>
   );
 }
