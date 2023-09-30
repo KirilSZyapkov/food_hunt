c6bffe6c4b01226c428c284a1b9353e8f89ef33c
Kiril Zyapkov <ziapkov@gmail.com>
2023-09-30 19:42:36 +0300

    Styling the recipe page. Adding the fetched date.

--- app/[id]/page.tsx
+++ app/[id]/page.tsx
@@ -4,15 +4,26 @@ import { fetchRecipeData } from "../../service/index";
 async function recipe({ params }: any) {
   const id = params.id;
   const recipe = await fetchRecipeData(id);
+  const instructions = recipe.instructions;
+  const ingredients = recipe.sections[0].components;
+
+  console.log(ingredients);
+  
 
   return (
-    <section>
-      <div>{recipe.name}</div>
-      <div>
-        <img src={recipe.thumbnail_url} alt="picture"/>
-        <div>продукти</div>
+    <section className="flex flex-col items-center bg-[#343a40] text-white">
+      <div className="text-7xl mt-[200px]">{recipe.name}</div>
+      <div className="flex pt-[100px]">
+        <img src={recipe.thumbnail_url} alt="picture" />
+        <div>{ingredients.map((i: any )=> (
+          <p>{i.raw_text}</p>
+        ))}</div>
       </div>
-      <div>инструкци за приготвяне</div>
+      <div>
+      {instructions?.map((i: any) => (
+        <p>{i.display_text}</p>
+      ))}
+      </div>
     </section>
   );
 }
