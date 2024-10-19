08d2c73d3f6517d2bc02f3c20e2d813198c13e5b
Kiril Zyapkov <ziapkov@gmail.com>
2024-10-19 21:20:04 +0300

    Fixing readability on home page.

--- components/landing/Landing.tsx
+++ components/landing/Landing.tsx
@@ -6,10 +6,13 @@ import Link from 'next/link';
 function Landing() {
   return (
     <div className={styles.header} id='/'>
-      <div className='text-current text-center text-[5.5rem]'>
-        <h1 className='font-bold my-4' >Food Hunt</h1>
-        <h2 className='text-[3.5rem] mb-[3rem]'>Always fresh &amp; Delightful</h2>
-        <Link className={`${styles.btn} ${styles.btn_lg} text-white bg-[#ff214f] border-[#ff214f]`} href="#gallery">View Our gallary</Link>
+      <div className="bg-slate-500/40 w-full h-full flex justify-center items-center">
+        <div className='text-current text-center text-[5.5rem]'>
+          <h1 className='font-bold my-4'>Food Hunt</h1>
+          <h2 className='text-[3.5rem] mb-[3rem]'>Always fresh &amp; Delightful</h2>
+          <Link className={`${styles.btn} ${styles.btn_lg} text-white bg-[#ff214f] border-[#ff214f]`} href="#gallery">View
+            Our gallary</Link>
+        </div>
       </div>
     </div>
   )
